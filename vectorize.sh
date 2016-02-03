75ed482044210f5c4111c1aa7b3850147da8efd7
Tim Peter <tim.peter@eonum.ch>
2016-02-03 17:14:57 +0100

    Use skip-n-gram model instead of CBOW and less dimensions.
    This lead to better (anectodal) results using word-analogy.

--- vectorize.sh
+++ vectorize.sh
@@ -1,2 +1,2 @@
 # mkdir data/vectorization if not already exists
-/home/tim/word2vec/word2vec -train data/tokenization/tokens.csv -output data/vectorization/vectors.csv -size 150 -save-vocab data/vectorization/vocab.csv -min-count 1
+/home/tim/word2vec/word2vec -train data/tokenization/tokens.csv -binary 1 -cbow 0 -output data/vectorization/vectors.bin -size 50 -save-vocab data/vectorization/vocab.csv -min-count 1
