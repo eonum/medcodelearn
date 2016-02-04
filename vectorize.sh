e0a34bac6cf4233b174083b7e78d643bee19297e
Tim Peter <tim.peter@eonum.ch>
2016-02-04 09:35:43 +0100

    Clean vectorize shell command.

--- vectorize.sh
+++ vectorize.sh
@@ -1,2 +1,2 @@
 # mkdir data/vectorization if not already exists
-/home/tim/word2vec/word2vec -train data/tokenization/tokens.csv -binary 1 -cbow 0 -output data/vectorization/vectors.bin -size 50 -save-vocab data/vectorization/vocab.csv -min-count 1
+word2vec -train data/tokenization/tokens.csv -binary 0 -cbow 0 -output data/vectorization/vectors.csv -size 50 -save-vocab data/vectorization/vocab.csv -min-count 1
