b2ecfd696f271f8fed9bea6201fe28cd0fc09744
Tim Peter <tim.peter@eonum.ch>
2016-02-04 16:31:18 +0100

    Use absolute imports in python 3.

--- vectorize.py
+++ vectorize.py
@@ -4,4 +4,4 @@ import os
 # mysteriously doesn't work anymore... instead using vectorize.sh at the moment.
 if __name__  == "__main__":
     datapath = os.path.join(os.path.dirname(__file__), 'data')
-    word2vec.word2vec(os.path.join(datapath,'tokenization', 'tokens.csv'), os.path.join(datapath,'vectorization', 'tokens.bin'), size=150, verbose=True)   
+    word2vec.word2vec(os.path.join(datapath,'tokenization', 'tokens.csv'), os.path.join(datapath,'vectorization', 'tokens.bin'), size=50, verbose=True)   
