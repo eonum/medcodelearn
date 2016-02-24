9c6f4d22c6556e3c8bc0d21f2d7074eb9153d911
Tim Peter <tim.peter@eonum.ch>
2016-02-24 18:50:40 +0100

    Stub fpr LSTM embedding classification.

---
+++ classification/lstm-embedding.py
@@ -0,0 +1,50 @@
+import numpy as np
+from keras.utils import np_utils
+
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Dropout, Dense
+import json
+from keras.callbacks import EarlyStopping
+from classification.LossHistoryVisualization import LossHistoryVisualisation
+
+def train_and_evaluate_lstm(config, X_train, X_test, y_train, y_test, output_dim, task):
+    y_train = np_utils.to_categorical(y_train, output_dim)
+    y_test = np_utils.to_categorical(y_test, output_dim)
+    
+    
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.15, random_state=23)
+   
+    # TODO normalization
+    
+    model = Sequential()
+    model.add(Embedding(num_features, 128, input_length=maxlen))
+    model.add(LSTM(output_dim=128, input_dim=X_train.shape[2], activation='sigmoid', inner_activation='hard_sigmoid'))
+    model.add(Dropout(0.5))
+    model.add(Dense(output_dim, activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy',
+                  class_mode='categorical',
+                  optimizer='adam')
+    
+    json.dump(json.loads(model.to_json()), 
+              open(config['base_folder'] + 'classification/model_lstm_' + task + '.json','w'), indent=4, sort_keys=True)   
+
+    
+    early_stopping = EarlyStopping(monitor='val_acc', patience=10)
+    visualizer = LossHistoryVisualisation(config['base_folder'] + 'classification/epochs_' + task + '.png')
+    model.fit(X_train, y_train,
+              nb_epoch=100,
+              batch_size=64,
+              show_accuracy=True,
+              validation_data=(X_validation, y_validation),
+              verbose=2,
+              callbacks=[early_stopping, visualizer])
+    
+    print("Prediction using LSTM..")
+    score = model.evaluate(X_test, y_test, show_accuracy=True, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])  
+
+    return [model, None, score[1]]
