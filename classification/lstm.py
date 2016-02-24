498a42ff9acd94cfa8214e11b0f6fe78387d7631
Tim Peter <tim.peter@eonum.ch>
2016-02-24 14:31:40 +0100

    Pad sequences.

--- classification/lstm.py
+++ classification/lstm.py
@@ -1,13 +1,46 @@
+import numpy as np
+from keras.utils import np_utils
 
+from sklearn.cross_validation import train_test_split
 
 def train_and_evaluate_lstm(config, X_train, X_test, y_train, y_test, output_dim, task):
     y_train = np_utils.to_categorical(y_train, output_dim)
     y_test = np_utils.to_categorical(y_test, output_dim)
     
+    X_train = pad_sequences(X_train, dim=len(X_train[0][0]))
+    X_test = pad_sequences(X_test, dim=len(X_train[0][0]))
+    
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.15, random_state=23)
-    scaler = preprocessing.MaxAbsScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    X_validation = scaler.transform(X_validation)
+   
 
     return [model, scaler, score]
+    
+def pad_sequences(sequences, maxlen=None, dim=1, dtype='float32',
+    padding='pre', truncating='post', value=0.):
+    '''
+        Override keras method to allow multiple feature dimensions.
+
+        @dim: input feature dimension (number of features per timestep)
+    '''
+    lengths = [len(s) for s in sequences]
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    x = (np.ones((nb_samples, maxlen, dim)) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError("Truncating type '%s' not understood" % padding)
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError("Padding type '%s' not understood" % padding)
+    return x
