820ffaabdd4cda260dbdcc03d5159d36aad14b08
Tim Peter <tim.peter@eonum.ch>
2016-03-09 20:06:00 +0100

    First try at converting model to graph.

--- classification/lstmembedding.py
+++ classification/lstmembedding.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.utils import np_utils
 
 from sklearn.cross_validation import train_test_split
-from keras.models import Sequential
+from keras.models import Graph
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dropout, Dense
 import json
@@ -25,16 +25,25 @@ def train_and_evaluate_lstm_with_embedding(config, X_train, X_test, y_train, y_t
             continue
         embedding_weights[index,:] = vector_by_token[word]
        
-    model = Sequential()
-    model.add(Embedding(n_symbols, config['word2vec-dim-size'], input_length=150, mask_zero=True, weights=[embedding_weights]))
+    model = Graph()
+    model.add_input(name='codes_input', input_shape=(n_symbols,), dtype='int')
+    model.add_node(Embedding(n_symbols, config['word2vec-dim-size'], input_length=150, mask_zero=True, weights=[embedding_weights]), name='embedding', input='codes_input')
+    node = 'embedding'
     for i, layer in enumerate(config['lstm-layers']):
-        model.add(LSTM(output_dim=layer['output-size'], activation='sigmoid', inner_activation='hard_sigmoid',  return_sequences=i != len(config['lstm-layers']) - 1))
-        model.add(Dropout(layer['dropout']))
+        inputnode = node
+        node = 'lstm' + str(i)
+        model.add_node(LSTM(output_dim=layer['output-size'], activation='sigmoid', 
+                            inner_activation='hard_sigmoid',
+                            return_sequences=i != len(config['lstm-layers']) - 1),
+                            name=node, input=inputnode)
+        inputnode = node
+        node = 'dropout' + str(i)
+        model.add_node(Dropout(layer['dropout']), name=node, input=inputnode)
     
-    model.add(Dense(output_dim, activation='softmax'))
+    model.add_node(Dense(output_dim, activation='softmax'), name='softmax', input=node)
+    model.add_output(name='output', input='softmax')
     
-    model.compile(loss='categorical_crossentropy',
-                  class_mode='categorical',
+    model.compile(loss={'output' : 'categorical_crossentropy'},
                   optimizer=config['optimizer'])
     
     json.dump(json.loads(model.to_json()), 
@@ -45,8 +54,8 @@ def train_and_evaluate_lstm_with_embedding(config, X_train, X_test, y_train, y_t
     visualizer = LossHistoryVisualisation(config['base_folder'] + 'classification/epochs_' + task + '.png')
     model.fit(X_train, y_train,
               nb_epoch=50,
-              batch_size=128,
-              show_accuracy=True,
+              #batch_size=128,
+              #show_accuracy=True,
               validation_data=(X_validation, y_validation),
               verbose=2,
               callbacks=[early_stopping, visualizer])
