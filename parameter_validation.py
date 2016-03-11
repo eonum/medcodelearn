4f7299efa8a2bc9de760814e12e7a8886e93445d
Tim Peter <tim.peter@eonum.ch>
2016-03-11 15:16:56 +0100

    Update parameter validation script.

--- parameter_validation.py
+++ parameter_validation.py
@@ -5,13 +5,24 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+def plot_scores(params, scores):
+    plt.plot(params, scores)
+    plt.title('validation on the size of the word2vec dimensionality')
+    plt.ylabel('accuracy')
+    plt.xlabel('vector size')
+    plt.grid(True)
+    plt.savefig(base_folder + 'word2vec_dimensions_validation.pdf')
+
 if __name__ == '__main__':
     base_folder = 'data/validate-word2vec-dimsize/'
     config = {
-              'base_folder' : base_folder,
+        'base_folder' : base_folder,
+        # skip the word2vec vectorization step. Only possible if vectors have already been calculated.
+        'skip-word2vec' : False,
+        # classifier, one of 'random-forest', 'ffnn' (feed forward neural net), 'lstm', 'lstm-embedding'
+        'classifier' : 'lstm-embedding',
         # Store all intermediate results. 
         # Disable this to speed up a run and to reduce disk space usage.
-        'classifier' : 'ffnn',
         'store-everything' : False,
         'drg-catalog' : 'data/2015/drgs.csv',
         'chop-catalog' : 'data/2015/chop_codes.csv',
@@ -19,11 +30,14 @@ if __name__ == '__main__':
         'drg-tokenizations' : base_folder + 'tokenization/drgs_tokenized.csv',
         'icd-tokenizations' : base_folder + 'tokenization/icd_codes_tokenized.csv',
         'chop-tokenizations' : base_folder + 'tokenization/chop_codes_tokenized.csv',
+        'use_demographic_tokens' : True,
+        # use skip grams (False) or CBOW (True) for word2vec
+        'word2vec-cbow' : True,
         # Use the code descriptions for tokenization
         'use-descriptions' : True,
         'use-training-data-for-word2vec' : True,
         'shuffle-word2vec-traindata' : True,
-        'num-shuffles' : 1,
+        'num-shuffles' : 10,
         'all-tokens' : base_folder + 'tokenization/all_tokens.csv',
         'code-tokens' : base_folder + 'tokenization/all_tokens_by_code.json',
         'all-vocab' : base_folder + 'tokenization/vocab_all.csv',
@@ -33,12 +47,23 @@ if __name__ == '__main__':
         'code-vectors' : base_folder + 'vectorization/all_vectors_by_code.json',
         'training-set-word2vec' : 'data/2015/trainingData2015_20151001.csv.last',
         'training-set' : 'data/2015/trainingData2015_20151001.csv.small',
-        'training-set-drgs' : 'data/2015/trainingData2015_20151001.csv.out.small',
+        'training-set-drgs' : 'data/2015/trainingData2015_20151001.csv.small.out',
         # word2vec is deterministic only if non-parallelized. (Set num-cores to 1)
         'num-cores' : 8,
         # which demographic variables should be used.
         # a subset from ['admWeight', 'hmv', 'sex', 'los', 'ageYears', 'ageDays']
-        'demo-variables' : [] }
+        'demo-variables' : ['admWeight', 'hmv', 'sex', 'los', 'ageYears', 
+                                               'ageDays', 'adm-normal', 'adm-transfer', 
+                                               'adm-transfer-short', 'adm-unknown',
+                                               'sep-normal', 'sep-dead', 'sep-doctor',
+                                               'sep-unknown', 'sep-transfer'],
+        # NN optimizer, one of ['sgd', 'rmsprop', 'adam', 'adagrad', 'adadelta', 'adamax']
+        'optimizer' : 'adam',
+        # Whether to use all tokens for the LSTM embedding or only codes (normalized sum over all vectors)
+        'use-all-tokens-in-embedding' : False,
+        # maximum sequence length for training
+        'maxlen' : 32,
+        'lstm-layers' : [{'output-size' : 128, 'dropout' : 0.1}] }
 
     
     if not os.path.exists(base_folder):
@@ -48,19 +73,16 @@ if __name__ == '__main__':
     
     scores = []
     params = []
-    for i in range(1, 150, 5):
+    for i in range(1, 250, 10):
         print("Validate parameter " + str(i))
         config['word2vec-dim-size'] = i
         score = run(config)
         scores.append(score)
         params.append(i)
+        plot_scores(params, scores)
     
     print(params)    
     print(scores)
     
-    plt.plot(params, scores)
-    plt.title('validation on the size of the word2vec dimensionality')
-    plt.ylabel('accuracy')
-    plt.xlabel('vector size')
-    plt.grid(True)
-    plt.savefig(base_folder + 'word2vec_dimensions_validation.pdf')
+    plot_scores(params, scores)
+  
