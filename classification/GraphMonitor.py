ca864bc12f26debf090fd63956d9c3a9f64c7c2d
Tim Peter <tim.peter@eonum.ch>
2016-03-11 16:30:32 +0100

    Adapt GraphMonitor for multi-input graphs.

--- classification/GraphMonitor.py
+++ classification/GraphMonitor.py
@@ -6,7 +6,6 @@ import keras
 import sys
 import numpy as np
 
-# TODO: Multiple inputs are not yet working.
 class GraphMonitor(keras.callbacks.Callback):
     
     def __init__(self, base_folder, task_name='', patience=10, output_names=[]):
@@ -16,6 +15,8 @@ class GraphMonitor(keras.callbacks.Callback):
         self.output_names = output_names
         
     def on_train_begin(self, logs={}):
+        if len(self.output_names) == 0:
+            self.output_names = self.model.output_order
         self.max_val_acc = 0.0
         self.max_epoch = 0
         
@@ -34,20 +35,20 @@ class GraphMonitor(keras.callbacks.Callback):
         self.X_val = {}
         self.y_val = {}
         
-        num_outlayers = len(self.model.output_order)
+        for i, input_layer in enumerate(self.model.input_order):
+            self.X_train[input_layer] = self.model.training_data[i]
+            self.X_val[input_layer] = self.model.validation_data[i]      
+        
+        num_inlayers = len(self.model.input_order)
         for out_layer in self.output_names:
-            out_index = self.model.output_order.index(out_layer)
-            
-            self.X_train[out_layer] = self.model.training_data[out_index]
-            # TODO check if this indexing is still valid with multiple inputs
-            temp = self.model.training_data[num_outlayers + out_index]
+            out_index = self.model.output_order.index(out_layer)      
+
+            temp = self.model.training_data[num_inlayers + out_index]
             self.y_train[out_layer] = np.empty(temp.shape[0], dtype=np.int32)
             for i in range(0, temp.shape[0]):
                 self.y_train[out_layer][i] = temp[i].argsort()[::-1][0]
             
-            
-            self.X_val[out_layer] = self.model.validation_data[out_index]
-            temp = self.model.validation_data[num_outlayers + out_index]
+            temp = self.model.validation_data[num_inlayers + out_index]
             print(temp.shape)
             self.y_val[out_layer] = np.empty(temp.shape[0], dtype=np.int32)
             for i in range(0, temp.shape[0]):
@@ -59,14 +60,8 @@ class GraphMonitor(keras.callbacks.Callback):
         self.epochs.append(epoch)
         
         for out_layer in self.output_names:            
-            X_train = self.X_train[out_layer]
-            y_train = self.y_train[out_layer]
-            
-            X_val = self.X_val[out_layer]
-            y_val = self.y_val[out_layer]
-            
-            acc_val = self.accuracy(X_val, y_val, out_layer)
-            acc_train = self.accuracy(X_train, y_train, out_layer)
+            acc_val = self.accuracy(self.X_val, self.y_val[out_layer], out_layer)
+            acc_train = self.accuracy(self.X_train, self.y_train[out_layer] , out_layer)
             print('train_acc: ' + str(acc_train) + ', val_acc: ' + str(acc_val))
         
             self.val_accs[out_layer].append(acc_val)
@@ -106,7 +101,7 @@ class GraphMonitor(keras.callbacks.Callback):
         sys.stdout.flush()
     
     def accuracy(self, X, y, out_layer):
-        probabs = self.model.predict({'codes_input':X}, verbose=0)[out_layer]
+        probabs = self.model.predict(X, verbose=0)[out_layer]
         acc = 0.0
         for i in range(0, probabs.shape[0]):
             label = probabs[i].argsort()[::-1][0]
